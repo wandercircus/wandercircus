1a43ebc4ef7609ac5135597dca4d80482adb08d3
Martin Kreichgauer <kreichgauer@gmail.com>
2011-08-27 19:05:42 +0200

    port 80 for production, privilege downgrading

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ var path = require('path');
 
 module.exports = {
     host: '0.0.0.0',
-    port: 3000,
+    port: process.env.NODE_ENV === 'production' ? 80 : 3000,
     skriptsPath: path.join(__dirname, 'skripts')
 };
+
+// if run as root, downgrade to the owner of this file
+if (process.getuid() === 0) {
+  require('fs').stat(__filename, function(err, stats) {
+    if (err) return console.log(err)
+    process.setuid(stats.uid);
+  });
+}
