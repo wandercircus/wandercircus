f482665384b3eaac4053a0b411c1bc0dc93dddce
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-08-28 01:08:06 +0200

    parseInt 0 instead of undefined

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ if (args.length > 0){
     console.log("Running with", args[0]);
     var theater = theaters.irc.getTheater();
     var hamlet = skripts[args[0]];
-    var scene = parseInt(args[1], 10);
+    var scene = parseInt(args[1] || 0, 10);
     var runSkript = hamlet.skript;
     if (scene !== undefined){
         console.log("Cut to", scene);
